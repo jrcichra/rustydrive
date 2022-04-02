d4091ec3e8084abeb4fd13f887cb78ba5c1a8f43
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-04-02 17:33:30 -0400

    try to disable cache

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use std::cmp::Ordering;
-use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::{self, Read, Seek, SeekFrom};
+use std::os::unix::io::AsRawFd;
 use std::process;
+const BUFFER_SIZE: u64 = 512;
 
-const BUFFER_SIZE: u64 = 1_000_000;
 fn main() -> io::Result<()> {
     let filename = std::env::args().nth(1).expect("Please supply a filename");
+
+    let mut f = OpenOptions::new()
+        .read(true)
+        // .custom_flags(libc::O_DIRECT)
+        .open(filename)
+        .expect("Failed to open file");
+
+    unsafe {
+        libc::posix_fadvise(f.as_raw_fd(), 0, 0, libc::POSIX_FADV_DONTNEED);
+    }
+
     let mut buffer: [Vec<u8>; 10] = Default::default();
     for i in 0..buffer.len() {
         buffer[i] = vec![0; BUFFER_SIZE.try_into().unwrap()];
     }
 
-    let mut f = File::open(filename)?;
     let mut location = 0;
     loop {
         f.seek(SeekFrom::Start(location))?;
@@ -26,13 +37,13 @@ fn main() -> io::Result<()> {
 
         for i in &buffer[1..] {
             if let Ordering::Less | Ordering::Greater = &buffer[0].cmp(i) {
-                println!("Byte mismatch starting at: {}MB", location / 1_000_000);
+                println!("Byte mismatch starting at: {}MB", location / BUFFER_SIZE);
                 process::exit(1);
             }
         }
 
         location += BUFFER_SIZE;
 
-        println!("{} MB", location / 1_000_000);
+        println!("{} MB", location / BUFFER_SIZE);
     }
 }
