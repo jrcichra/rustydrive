57b9806476cf2f5906cb696819dc266c6bca8860
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-04-03 12:07:31 -0400

    align vector to 4096 boundary for O_DIRECT

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # rustydrive [![Rust](https://github.com/jrcichra/rustydrive/actions/workflows/rust.yml/badge.svg)](https://github.com/jrcichra/rustydrive/actions/workflows/rust.yml)
 
-Reads a drive 10 times, 1 megabyte at a time and stops if there are any differences
-
-# NOTE
-
-Struggling to have linux (actually) read the drive 10 times without caching. `O_DIRECT` gives `Invalid argument` errors no matter what block size I use. Currently I have `posix_fadvise` `POSIX_FADV_DONTNEED` in place. But this isn't guaranteed to read the drive N times.
+Reads a drive 10 times, 1 megabyte at a time and stops if there are any differences. Works without caching by using `O_DIRECT` and aligned vectors.
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
+use maligned::align_first;
 use std::cmp::Ordering;
 use std::fs::OpenOptions;
 use std::io::{self, Read, Seek, SeekFrom};
-use std::os::unix::io::AsRawFd;
+use std::os::unix::fs::OpenOptionsExt;
 use std::process;
-const BUFFER_SIZE: u64 = 1_000_000;
+
+const READ_ATTEMPTS: usize = 10;
+const BUFFER_SIZE: u64 = 4096;
 
 fn main() -> io::Result<()> {
     let filename = std::env::args().nth(1).expect("Please supply a filename");
 
     let mut f = OpenOptions::new()
         .read(true)
-        // .custom_flags(libc::O_DIRECT)
+        .custom_flags(libc::O_DIRECT)
         .open(filename)
         .expect("Failed to open file");
 
-    unsafe {
-        libc::posix_fadvise(f.as_raw_fd(), 0, 0, libc::POSIX_FADV_DONTNEED);
-    }
-
-    let mut buffer: [Vec<u8>; 10] = Default::default();
+    let mut buffer: [Vec<u8>; READ_ATTEMPTS] = Default::default();
     for i in 0..buffer.len() {
-        buffer[i] = vec![0; BUFFER_SIZE.try_into().unwrap()];
+        buffer[i] = align_first::<u8, maligned::A4096>(BUFFER_SIZE.try_into().unwrap());
+        for _ in 0..BUFFER_SIZE {
+            buffer[i].push(0);
+        }
     }
 
     let mut location = 0;
@@ -29,7 +31,7 @@ fn main() -> io::Result<()> {
         for i in &mut buffer {
             let num = f.read(i)?;
             if num <= 0 {
-                println!("Reached end of file");
+                println!("f.read returned {}", num);
                 process::exit(0);
             }
             f.seek(SeekFrom::Start(location))?;
@@ -37,13 +39,13 @@ fn main() -> io::Result<()> {
 
         for i in &buffer[1..] {
             if let Ordering::Less | Ordering::Greater = &buffer[0].cmp(i) {
-                println!("Byte mismatch starting at: {}MB", location / BUFFER_SIZE);
+                println!("Byte mismatch starting at: {}", location);
                 process::exit(1);
             }
         }
 
         location += BUFFER_SIZE;
 
-        println!("{} MB", location / BUFFER_SIZE);
+        println!("{} MB", location);
     }
 }
